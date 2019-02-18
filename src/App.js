8c38ef5baaa885da9fe4b1093ef8bd50a216bb1b
Phong Ly <phongly@Phong-MBP.local>
2019-02-18 18:04:08 +0700

    first sight of tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component} from "react";
 import "./App.css";
-
-class App extends Component{
+import Game from "./tic-tac-toe/TicTacToe.js"
+class App extends Component {
   render(){
     return(
       <div className="App">
         <h1> Hello, World! </h1>
+        <Game />
       </div>
     );
   }
